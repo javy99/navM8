1f65ab1fac1826ce60db10fecded058b7a714d5b
Javlonbek <jkosimov@nokia.com>
2024-05-19 23:49:35 +0200

    cookie related configs

--- backend/middlewares/requireAuth.ts
+++ backend/middlewares/requireAuth.ts
@@ -34,6 +34,7 @@ const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
           httpOnly: true,
           secure: true,
           maxAge: 3 * 24 * 60 * 60 * 1000,
+          domain: '.herokuapp.com',
           sameSite: 'none',
         })
 
